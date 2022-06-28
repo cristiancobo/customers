eb9d38f12ecf313111e4bc3d609f55cd45056e60
Cristian Cobo Ramirez <cristian.cobor@perficient.com>
2022-06-28 17:13:23 -0500

    files

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -5,13 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/amor-de-mi-vida")
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class CustomerController {
 
     @GetMapping("/")
     public ResponseEntity<String> findAll(){
-        String customer = "sebastian";
+        String customer = "eres mi sol que ilumina mi vida.";
         return new ResponseEntity<String>(customer, HttpStatus.OK);
     }
 }
